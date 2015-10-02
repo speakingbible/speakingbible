1d29d756e47dbe88ca8337f3507d7a0ec515c26f
Speaking Bible <speakingbible@speakingbible.org>
2015-10-02 07:50:27 -0400

    Broken Form fix

--- site/js/contact_me.js
+++ site/js/contact_me.js
@@ -1,6 +1,6 @@
 $(function() {
 
-    $("input,textarea").jqBootstrapValidation({
+ /*   $("input,d").jqBootstrapValidation({
         preventSubmit: true,
         submitError: function($form, event, errors) {
             // additional error messages or events
@@ -15,12 +15,12 @@ $(function() {
             var email = $("input#email").val();
             var phone = $("input#phone").val();
             var message = $("textarea#message").val();
-            var firstName = name; // For Success/Failure Message
+            /*var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
-            }
-            $.ajax({
+            }*/
+          /*  $.ajax({
                 url: "././mail/contact_me.php",
                 type: "POST",
                 data: {
@@ -64,10 +64,10 @@ $(function() {
     $("a[data-toggle=\"tab\"]").click(function(e) {
         e.preventDefault();
         $(this).tab("show");
-    });
+    });*/
 });
 
 // When clicking on Full hide fail/success boxes
 $('#name').focus(function() {
-    $('#success').html('');
+    $('#success').html(''); 
 });
